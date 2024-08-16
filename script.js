adf37fe3224b6de985f0cc05f4974ad71a312649
vrindabajaj <78031928+vrindabajaj@users.noreply.github.com>
2024-08-16 11:55:12 +0100

    Create form and dialog box

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const library = document.querySelector(".library-container");
+const modal = document.querySelector(".modal");
 const add_book = document.querySelector(".add-book");
+const form_submit = document.querySelector(".form-submit");
+const span = document.querySelector(".close");
+// const book = document.createElement("div");
+// book.classList.add("book");
 
 add_book.addEventListener("click", () => {
-    const book = document.createElement("div");
-    book.classList.add("book");
-    library.appendChild(book);
+    modal.style.display = "block";
 })
+
+form_submit.addEventListener("click", () => {
+    //create new Book object using form inputs and add to array + display
+    modal.style.display = "none";
+})
+
+span.addEventListener("click", () => {
+    modal.style.display = "none";
+})
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
